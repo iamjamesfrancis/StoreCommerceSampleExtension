5fd567ef110db338c3e2e86fa9e3c20c6768627d
James Francis <jamesfrancis1947@gmail.com>
2022-11-07 11:56:16 +0530

    Added Sample Extension

---
+++ src/Sample/Solutions/TestExtension/CommerceRuntime/DefinePosExtensionPackageTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommerceRuntime
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Dynamics.Commerce.Runtime;
+    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+    using Microsoft.Dynamics.Commerce.Runtime.Messages;
+
+/// <summary>
+/// Class that implements a post trigger for the GetExtensionPackageDefinitionsRequest request type.
+/// </summary>
+public class DefinePosExtensionPackageTrigger : IRequestTrigger
+{
+    /// <summary>
+    /// Gets the supported requests for this trigger.
+    /// </summary>
+    public IEnumerable<Type> SupportedRequestTypes
+    {
+        get
+        {
+            return new[] { typeof(GetExtensionPackageDefinitionsRequest) };
+        }
+    }
+
+    /// <summary>
+    /// Post trigger to retrieve extension package.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="response">The response.</param>
+    public void OnExecuted(Request request, Response response)
+    {
+        var getExtensionsResponse = (GetExtensionPackageDefinitionsResponse)response;
+        var extensionPackageDefinition = new ExtensionPackageDefinition();
+
+        // Should match the PackageName used when packaging the customization package.
+        extensionPackageDefinition.Name = "Alpha Variance Solutions.TestExtension";
+        extensionPackageDefinition.Publisher = "Alpha Variance Solutions";
+        extensionPackageDefinition.IsEnabled = true;
+
+        getExtensionsResponse.ExtensionPackageDefinitions.Add(extensionPackageDefinition);
+    }
+
+    public void OnExecuting(Request request)
+    {
+    }
+}
+}
---
+++ src/Sample/readme.md
@@ -0,0 +1,14 @@
+# Store Commerce Samples
+## Overview
+This folder contains sample solutions that demonstrate how to customize the Store Commerce (and POS) App in various scenarios. Each sample contains a solution containing a Commerce Runtime project to enable the POS extension package and the POS extension package with the sample extensions. The samples are organized based on the type of customizations.
+
+- Create: These extensions supplement POS by introducing new pages, dialogs or workflows that do not exist “out of the box”
+- Extend: These extensions modify POS by adding additional functionality to existing pages, workflows or other functionality
+- Solutions: These samples showcase a variety of extension types to enable a more complete feature or solution.
+
+## Samples List
+- Create/
+  - Dialogs/
+    - [BarcodeMsrDialogSample](./Create/Dialogs/BarcodeMsrDialogSample/readme.md)
+- Solutions/
+  - [GasStationSample](./Solutions/GasStationSample/README.md)
